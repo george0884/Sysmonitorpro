340bd6722384b6556165ebed20af196a584a3c63
Old The One Deluxe <georgecabrera08@gmail.com>
2026-05-08 22:41:31 -0300

    Delete first-run.sh

--- first-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# scripts/first-run.sh
-mkdir -p ~/.config/sysmonitorpro
-if [ ! -f ~/.config/sysmonitorpro/config.json ]; then
-    cp config/default.json ~/.config/sysmonitorpro/config.json
-    echo "Configuración creada en ~/.config/sysmonitorpro/config.json"
-fi
